1b5b2850998e0bae8f33b3e7d264fa62ac28dc7d
TejaSaii <tejas7833@gmail.com>
2022-10-29 11:57:16 +0530

    added Employee RestController and properties file

---
+++ src/main/java/com/spring/firstAPI/EmployeeManagementSystem/rest/EmployeeRestController.java
@@ -0,0 +1,61 @@
+package com.spring.firstAPI.EmployeeManagementSystem.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.firstAPI.EmployeeManagementSystem.entity.Employee;
+import com.spring.firstAPI.EmployeeManagementSystem.service.EmployeeService;
+
+@RestController
+@RequestMapping("/firstApi")
+public class EmployeeRestController {
+
+	//inject the service layer
+	@Autowired
+	public EmployeeService employeeService;
+	
+	@GetMapping("/employees")
+	public List<Employee> findAll(){
+		return employeeService.findAll();
+	}
+	
+	@GetMapping("/employee/{employeeId}")
+	public Employee findById(@PathVariable int employeeId) {
+		Employee theEmployee = employeeService.findById(employeeId);
+		
+		if(theEmployee == null) {
+			throw new RuntimeException("Employee Id not found - "+employeeId);
+		}
+		
+		return theEmployee;
+	}
+	
+	@PostMapping("/employees")
+	public Employee save(@RequestBody Employee theEmployee) {
+		theEmployee.setId(0);
+		employeeService.save(theEmployee);
+		return theEmployee;
+	}
+	
+	@PutMapping("/employees")
+	public Employee update(@RequestBody Employee theEmployee) {
+		employeeService.save(theEmployee);
+		return theEmployee;
+	}
+	
+	@DeleteMapping("/employees/{employeeId}")
+	public String delete(@PathVariable int employeeId) {
+		employeeService.deleteById(employeeId);
+		return "Deleted Employee with Id - "+employeeId;
+	}
+	
+}
